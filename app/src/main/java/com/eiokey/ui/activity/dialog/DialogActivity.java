71e16aa4434083038ed9daf15bfba56d96c2fe16
JetQiao <qiao522600806@126.com>
2021-03-18 10:35:20 +0800

    no message

---
+++ app/src/main/java/com/eiokey/ui/activity/dialog/DialogActivity.java
@@ -0,0 +1,29 @@
+package com.eiokey.ui.activity.dialog;
+
+import android.os.Bundle;
+
+import com.eiokey.library.activity.BaseActivity;
+
+/**
+ * description
+ * param
+ * return
+ *
+ * @author JetQiao
+ * @version 1.0.0
+ * @Time 2021/3/18 0018 10:33
+ */
+public class DialogActivity extends BaseActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initView()
+    {
+
+    }
+}
